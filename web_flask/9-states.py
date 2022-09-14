237584b63ff05a1effeb9ccd63777d82eddcc35f
Carlos Berrio <4634@holbertonschool.com>
2022-09-14 15:30:21 -0500

    task10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
 
-from logging import exception
-from os import stat_result
 from flask import Flask, render_template
 from models import storage
 from models import *
@@ -18,7 +16,7 @@ def states(state_id=None):
     """
     states = storage.all("State")
     if state_id is not None:
-        state = 'State.' + state_id
+        state_id = 'State.' + state_id
         return render_template('9-states.html', states=states, state_id=state_id)
 
 
