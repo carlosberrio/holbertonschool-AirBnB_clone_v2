a02d196cee5a14b9de818075bbb49da5fc9d226b
Carlos Berrio <4634@holbertonschool.com>
2022-09-14 15:07:48 -0500

    task9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,6 +6,7 @@ from models import storage
 from models import *
 app = Flask(__name__)
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """cities_by_states
@@ -15,12 +16,14 @@ def cities_by_states():
     states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """teardown_db
     Function that removes the current SQLAlchemy Session
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
