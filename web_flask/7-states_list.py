ad369761ee7f0f58bf3181d67e122a1566d364cb
Carlos Berrio <4634@holbertonschool.com>
2022-09-14 14:44:57 -0500

    task8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,12 +18,14 @@ def states_list():
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """teardown_db
     Function that removes the current SQLAlchemy Session
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
